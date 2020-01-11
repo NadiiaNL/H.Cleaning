69311e7ffa2bb7e989a36400cb7240487793f046
Nadiia Kozhushko <kozhushko51@gmail.com>
2020-01-12 00:09:34 +0200

    creating intro section with slider

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ function myFunction(x) {
 		y.className = 'header__menu menu';
 	}
 }
+$('.intro__slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	speed: 500,
+	fade: true,
+	dots: true,
+	arrows: false
+});
